9839e64c9470d8e5e100acbd3fdc5b9bc6897733
dmirandam <dmirandam@unal.edu.co>
2025-11-20 20:19:06 -0500

    add crossplane stuff

--- Readme.md
+++ Readme.md
@@ -1,4 +1,31 @@
+# Roles
 
 
+### Cluster (ok)
 
+- eksctl-santiago-karpenter-demo-template-ServiceRole-QtcJFKDy5Sn4 (service-role-%s) ok
 
+### NodeGroup (ok)
+
+- eksctl-santiago-karpenter-demo-tem-NodeInstanceRole-gczmHBV9um7y (node-role-%s) ok
+
+### aws-auth configmap
+
+- KarpenterNodeRole-santiago-karpenter-demo-template (karpenter-node-role-%s)
+- eksctl-santiago-karpenter-demo-tem-NodeInstanceRole-gczmHBV9um7y (node-role-%s)
+
+### Addon CNI (ok)
+
+- eksctl-santiago-karpenter-demo-template-addon-Role1-9SOAwCjpJRVb (addon-role-%s) ok
+
+## Access
+
+### IAM access entries
+
+- aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS ---
+- eksctl-santiago-karpenter-demo-tem-NodeInstanceRole-gczmHBV9um7y (node-role-%s) ok
+- user/CLI-admin ---
+
+### Pod Identity associations (ok)
+
+- santiago-karpenter-demo-template-karpenter (karpenter-controller-role-%s) ok
--- config.sh
+++ config.sh
@@ -60,6 +60,10 @@ run_step() {
 	success "Completed: ${msg}"
 }
 
+finish() {
+	echo -e "${BOLD}${GREEN}=== All steps completed successfully ===${RESET}"
+}
+
 header "Crossplane installation and AWS compositions"
 
 install_crossplane() {
@@ -77,16 +81,25 @@ run_step "Install Crossplane (helm)" install_crossplane
 run_step "Apply AWS functions" kubectl apply -f ./crossplane/AWS/functions.yaml
 run_step "Apply AWS secret" kubectl apply -f ./crossplane/AWS/secret.yaml
 run_step "Apply AWS providers" kubectl apply -f ./crossplane/AWS/providers.yaml
+
+run_step "Wait for AWS EKS provider to be Healthy" kubectl wait provider.pkg.crossplane.io/upbound-provider-aws-eks --for=condition=Healthy=True --timeout=240s
+run_step "Wait for AWS EC2 provider to be Healthy" kubectl wait provider.pkg.crossplane.io/upbound-provider-aws-ec2 --for=condition=Healthy=True --timeout=240s
+run_step "Wait for AWS IAM provider to be Healthy" kubectl wait provider.pkg.crossplane.io/upbound-provider-aws-iam --for=condition=Healthy=True --timeout=240s
+run_step "Wait for AWS CloudWatch provider to be Healthy" kubectl wait provider.pkg.crossplane.io/upbound-provider-aws-cloudwatchevents --for=condition=Healthy=True --timeout=240s
+run_step "Wait for AWS SQS provider to be Healthy" kubectl wait provider.pkg.crossplane.io/upbound-provider-aws-sqs --for=condition=Healthy=True --timeout=240s
+run_step "Wait for AWS Family provider to be Healthy" kubectl wait provider.pkg.crossplane.io/upbound-provider-family-aws --for=condition=Healthy=True --timeout=240s
+
+
 run_step "Apply AWS provider config (namespace: crossplane-system)" kubectl apply -f ./crossplane/AWS/provider_config.yaml -n crossplane-system
 run_step "Apply AWS XRD" kubectl apply -f ./crossplane/AWS/XRD.yaml
-run_step "Wait for AWS XRD to be established" timeout 120 bash -c 'until kubectl get xrd awsinfra.aws.kfo.io -o jsonpath="{.status.conditions[?(@.type==\"Established\")].status}" | grep True >/dev/null 2>&1; do sleep 5; done' #TODO: dobleckeck this condition
+run_step "Wait for AWS XRD to be established" timeout 120 bash -c 'until kubectl get xrd awsinfras.aws.kfo.io -o jsonpath="{.status.conditions[?(@.type==\"Established\")].status}" | grep True >/dev/null 2>&1; do sleep 5; done' #TODO: dobleckeck this condition
 
 header "Apply AWS Compositions"
 run_step "Apply VPC composition" kubectl apply -f ./crossplane/AWS/Compositions/vpc.yaml
 run_step "Apply IAM composition" kubectl apply -f ./crossplane/AWS/Compositions/iam.yaml
 run_step "Apply SQS composition" kubectl apply -f ./crossplane/AWS/Compositions/sqs.yaml
 run_step "Apply EKS composition" kubectl apply -f ./crossplane/AWS/Compositions/eks.yaml
-run_step "Apply Controller Role composition" kubectl apply -f ./crossplane/AWS/Compositions/controller.yaml
+#run_step "Apply Controller Role composition" kubectl apply -f ./crossplane/AWS/Compositions/controller.yaml
 
 header "Installing Tekton"
 run_step "Apply Tekton installation" kubectl apply -f https://storage.googleapis.com/tekton-releases/operator/latest/release.yaml
@@ -95,8 +108,9 @@ run_step "Apply Tekton config" kubectl apply -f https://raw.githubusercontent.co
 success "Tekton installed and configured."
 
 header "Tekton Dashboard"
-# wait for Tekton Dashboard to be ready
+run_step "Wait for tekton-pipelines namespace to be created" timeout 120 bash -c 'until kubectl get namespace tekton-pipelines >/dev/null 2>&1; do sleep 5; done'
+run_step "Wait for Tekton Dashboard deployment to be created" timeout 600 bash -c 'until kubectl get deployment tekton-dashboard -n tekton-pipelines >/dev/null 2>&1; do sleep 5; done'
 run_step "Wait for Tekton Dashboard to be ready" kubectl wait --for=condition=Available=True --timeout=300s deployment/tekton-dashboard -n tekton-pipelines
 run_step "Portforward Tekton Dashboard" kubectl port-forward -n tekton-pipelines svc/tekton-dashboard 9097:9097 &
 
-info "All steps finished."
+finish
